d6fed464b09b8eb9b232b36c71ffabb1a53c8139
Tgey <thomas.gey@epitech.eu>
2019-04-09 23:35:09 +0200

    MIT License + API codestyle + Makefile improvement + CI pylint recursive

--- api/app.py
+++ api/app.py
@@ -2,20 +2,17 @@
 this script represents a machine learning API
 """
 
-import os
 import pickle
 from flask import Flask
 from flask import request
 from sklearn import linear_model
 
 
-# creating and saving some model
 reg_model = linear_model.LinearRegression()
 reg_model.fit([[1., 1., 5.], [2., 2., 5.], [3., 3., 1.]], [0., 0., 1.])
 pickle.dump(reg_model, open('some_model.pkl', 'wb'))
-
-debug = os.environ.get('DEBUG', False)
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -23,6 +20,7 @@ def index():
         main api route
     """
     return "true"
+
 
 @app.route('/api', methods=['GET'])
 def get_prediction():
